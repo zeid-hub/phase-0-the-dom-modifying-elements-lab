f7aa8f9fdaf8e710981e1ed4d6a51d00cc1c0df0
zeid <zeidhassan4@gmail.com>
2024-01-15 18:45:50 +0300

    Complited assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const body = document.getElementsByTagName("body");
+const secondChild = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Peter is the champion";
